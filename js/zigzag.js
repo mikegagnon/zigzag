6bfe1a5ebdbd556b2ffacedee880ef392e7ad065
Michael Gagnon <mikegagnon@gmail.com>
2019-04-30 16:40:42 -0700

    draw cells

--- js/zigzag.js
+++ js/zigzag.js
@@ -46,10 +46,29 @@ var DOWN = 1;
 var LEFT = 2;
 var RIGHT = 3;
 
+var STATE_OFF = 0;
+var STATE_ON = 1;
+
+var COLOR_RED = "#f45042"
+var COLOR_WHITE = "white"
+
 var GRID_STROKE = "#ccc";
 
-var Particle = function(direction) {
+var Particle = function(r, c, direction) {
+    this.r = r;
+    this.c = c;
     this.direction = direction;
+}
+
+function newMatrix(rows, cols) {
+    var matrix = [];
+    for (var r = 0; r < rows; r++) {
+        matrix.push([]);
+        for (var c = 0; c < cols; c++) {
+            matrix[r].push(null);
+        }
+    }
+    return matrix;
 }
 
 var ZigzagSim = function(args) {
@@ -57,13 +76,13 @@ var ZigzagSim = function(args) {
     this.numCols = args.numCols;
     this.numParticles = args.numParticles;
 
-    this.matrix = [];
-    for (var r = 0; r < this.numRows; r++) {
+    this.matrix = newMatrix(this.numRows, this.numCols);
+    /*for (var r = 0; r < this.numRows; r++) {
         this.matrix.push([]);
         for (var c = 0; c < this.numCols; c++) {
             this.matrix[r].push(null);
         }
-    }
+    }*/
 
     for (var i = 0; i < this.numParticles; i++) {
         var r;
@@ -75,7 +94,7 @@ var ZigzagSim = function(args) {
 
         var direction = randDirection();
 
-        var p = new Particle(direction);
+        var p = new Particle(r, c, direction);
         this.matrix[r][c] = p;
     }
 };
@@ -87,16 +106,15 @@ var ZigzagViz = function(args) {
     this.height = args.height;
     this.numRows = args.numRows;
     this.numCols = args.numCols;
-    this.cellWidth = this.width / this.numCols;
-    this.cellHeight = this.height / this.numRows;
+    this.cellWidth = retina(this.width / this.numCols);
+    this.cellHeight = retina(this.height / this.numRows);
 
     this.canvas = this.addCanvas();
     this.stage = this.addStage();
 
+    this.cells = this.drawCells();
+
     this.drawGrid();
-
-    this.stage.update();
-
 }
 
 function retina(value) {
@@ -114,8 +132,8 @@ ZigzagViz.prototype.addStage = function() {
 ZigzagViz.prototype.addCanvas = function() {
 
 
-    var width = this.width; //Math.round(retina(this.width));
-    var height = this.height; //Math.round(retina(this.height));
+    var width = this.width;
+    var height = this.height;
 
     var canvasHtml = `
       <div class="window">
@@ -144,24 +162,44 @@ ZigzagViz.prototype.addCanvas = function() {
     return canvas;
 };
 
+ZigzagViz.prototype.drawCells = function() {
+    var matrix = newMatrix(this.numRows, this.numCols);
+
+    for (var r = 0; r < this.numRows; r++) {
+        for (var c = 0; c < this.numCols; c++) {
+            var x = c * this.cellWidth;
+            var y = r * this.cellHeight;
+            var shape = new createjs.Shape();
+            shape.graphics.beginFill(COLOR_WHITE).drawRect(x, y, this.cellWidth, this.cellHeight).endFill();
+            this.stage.addChild(shape);
+            matrix[r][c] = {
+                state: STATE_OFF,
+                shape: shape
+            }
+        }
+    }
+
+    return matrix;
+}
+
 ZigzagViz.prototype.drawGrid = function() {
 
     // Draw the horizontal lines
     for (var r = 1; r < this.numRows; r++) {
         this.drawLine(
             0,
-            Math.floor(retina(r * this.cellHeight)),
-            Math.floor(retina(this.numCols * this.cellWidth)),
-            Math.floor(retina(r * this.cellHeight)),
+            Math.floor(r * this.cellHeight),
+            Math.floor(this.numCols * this.cellWidth),
+            Math.floor(r * this.cellHeight),
             GRID_STROKE);
     }
     
     for (var c = 1; c < this.numCols; c++) {
         this.drawLine(
-            Math.floor(retina(c * this.cellWidth)),
+            Math.floor(c * this.cellWidth),
             0,
-            Math.floor(retina(c * this.cellWidth)),
-            Math.floor(retina(this.numRows * this.cellHeight)),
+            Math.floor(c * this.cellWidth),
+            Math.floor(this.numRows * this.cellHeight),
             GRID_STROKE);
     }
 };
@@ -178,8 +216,26 @@ ZigzagViz.prototype.drawLine = function(x1, y1, x2, y2, color) {
     this.stage.addChild(line);
 }
 
+ZigzagViz.prototype.update = function(sim) {
+    for (var r = 0; r < this.numRows; r++) {
+        for (var c = 0; c < this.numCols; c++) {
+            var cell = this.cells[r][c];
+            var simState = sim.matrix[r][c] == null ? STATE_OFF : STATE_ON;
+            if (simState != cell.state) {
+                var color = simState == STATE_OFF ? COLOR_WHITE : COLOR_RED;
+                var x = c * this.cellWidth;
+                var y = r * this.cellHeight;
+                cell.shape.graphics.clear().beginFill(color).drawRect(x, y, this.cellWidth, this.cellHeight).endFill();
+            }
+        }
+    }
+    this.stage.update();
+}
+
 var ZigzagSimViz = function(args) {
     this.simId = args.simId;
     this.simulator = new ZigzagSim(args);
     this.viz = new ZigzagViz(args);
+
+    this.viz.update(this.simulator);
 };
