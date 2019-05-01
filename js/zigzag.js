c7cf04ee243fc2212c49275c6dceccfd1b1370a2
Michael Gagnon <mikegagnon@gmail.com>
2019-04-30 17:26:58 -0700

    gliders

--- js/zigzag.js
+++ js/zigzag.js
@@ -18,6 +18,45 @@ function clickReset(id) {
     instances[id].clickReset();
 }
 
+function eventTick(id) {
+    instances[id].eventTick();
+}
+
+function initializeTicker() {
+    createjs.Ticker.addEventListener("tick", tick)
+    createjs.Ticker.setFPS(FPS)
+
+    // temporary
+    createjs.Ticker.paused = false;
+}
+
+initializeTicker();
+
+function tick(event) {
+
+    if (createjs.Ticker.paused || currentInstance == null) {
+      return false;
+    }
+
+    //val rd = instances(activeInstanceId.get)
+    //rd.viz.tick(event)
+    //instances[currentInstance].tick(event);
+    eventTick(currentInstance);
+    return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // TODO: mersenne twister
 var seed = 3;
 function random() {
@@ -54,10 +93,113 @@ var COLOR_WHITE = "white"
 
 var GRID_STROKE = "#ccc";
 
-var Particle = function(r, c, direction) {
-    this.r = r;
-    this.c = c;
+var FPS = 30;
+
+P_MOVE = 0;
+P_TURN_LEFT = 1;
+P_TURN_RIGHT = 2;
+
+
+var ROTATE_LEFT = {
+    0: LEFT,
+    1: RIGHT,
+    2: DOWN,
+    3: UP
+};
+
+var ROTATE_RIGHT = {
+    0: RIGHT,
+    1: LEFT,
+    2: UP,
+    3: DOWN
+};
+
+
+
+var Particle = function(sim, r, c, direction) {
+    this.sim = sim;
+    this.row = r;
+    this.col = c;
     this.direction = direction;
+    this.instructionPointer = 0;
+
+    this.program = [
+        P_MOVE,
+        P_TURN_LEFT,
+        P_MOVE,
+        P_TURN_RIGHT
+    ];
+}
+
+Particle.prototype.step = function() {
+    var instruction = this.program[this.instructionPointer];
+
+    if (instruction == P_MOVE) {
+        var newRc = this.dirRowCol(this.direction, this.row, this.col);
+        var r = newRc.row;
+        var c = newRc.col;
+
+        if (this.sim.matrix[r][c] == null) {
+            this.sim.matrix[this.row][this.col] = null;
+            this.sim.matrix[r][c] = this;
+            this.row = r;
+            this.col = c;
+        }
+
+    } else if (instruction == P_TURN_LEFT) {
+        this.direction = ROTATE_LEFT[this.direction];
+    } else if (instruction == P_TURN_RIGHT) {
+        this.direction = ROTATE_RIGHT[this.direction];
+    } else {
+        throw "Shouldn't happen";
+    }
+
+    this.instructionPointer = (this.instructionPointer + 1) % this.program.length;
+}
+
+Particle.prototype.dirRowCol = function(direction, row, col) {
+
+    if (row < 0 || row >= this.sim.numRows || col < 0 || col >= this.sim.numCols) {
+      throw "row, col is out of bounds";
+    }
+
+    var rc;
+
+    if (direction == UP) {
+        rc = {
+            row: row - 1,
+            col: col
+        };
+    } else if (direction == DOWN) {
+        rc = {
+            row: row + 1,
+            col: col
+        };
+    } else if (direction == LEFT) {
+        rc = {
+            row: row,
+            col: col - 1
+        };
+    } else if (direction == RIGHT) {
+        rc = {
+            row: row,
+            col: col + 1
+        };
+    } else {
+        throw "Shouldn't happen";
+    }
+
+    if (rc.row == -1) {
+        rc.row = this.sim.numRows - 1;
+    } else if (rc.row == this.sim.numRows) {
+        rc.row = 0
+    } else if (rc.col == -1)  {
+        rc.col = this.sim.numCols - 1;
+    } else if (rc.col == this.sim.numCols) {
+        rc.col = 0;
+    }
+
+    return rc;
 }
 
 function newMatrix(rows, cols) {
@@ -75,14 +217,8 @@ var ZigzagSim = function(args) {
     this.numRows = args.numRows;
     this.numCols = args.numCols;
     this.numParticles = args.numParticles;
-
     this.matrix = newMatrix(this.numRows, this.numCols);
-    /*for (var r = 0; r < this.numRows; r++) {
-        this.matrix.push([]);
-        for (var c = 0; c < this.numCols; c++) {
-            this.matrix[r].push(null);
-        }
-    }*/
+    this.particles = [];
 
     for (var i = 0; i < this.numParticles; i++) {
         var r;
@@ -91,13 +227,18 @@ var ZigzagSim = function(args) {
             r = randInt(this.numRows);
             c = randInt(this.numCols);
         } while (this.matrix[r][c] != null);
-
         var direction = randDirection();
-
-        var p = new Particle(r, c, direction);
+        var p = new Particle(this, r, c, direction);
         this.matrix[r][c] = p;
+        this.particles.push(p);
     }
 };
+
+ZigzagSim.prototype.step = function() {
+    for (var i = 0; i < this.numParticles; i++) {
+        this.particles[i].step(this);
+    }
+}
 
 var ZigzagViz = function(args) {
     this.simId = args.simId;
@@ -222,6 +363,7 @@ ZigzagViz.prototype.update = function(sim) {
             var cell = this.cells[r][c];
             var simState = sim.matrix[r][c] == null ? STATE_OFF : STATE_ON;
             if (simState != cell.state) {
+                cell.state = simState;
                 var color = simState == STATE_OFF ? COLOR_WHITE : COLOR_RED;
                 var x = c * this.cellWidth;
                 var y = r * this.cellHeight;
@@ -234,8 +376,26 @@ ZigzagViz.prototype.update = function(sim) {
 
 var ZigzagSimViz = function(args) {
     this.simId = args.simId;
+    instances[this.simId] = this;
+
+    // temporary
+    currentInstance = this.simId;
+
+    this.stepsPerTick = args.stepsPerTick;
+
     this.simulator = new ZigzagSim(args);
     this.viz = new ZigzagViz(args);
 
     this.viz.update(this.simulator);
 };
+
+
+ZigzagSimViz.prototype.eventTick = function(event) {
+    for (var i = 0; i < this.stepsPerTick; i++) {
+        this.simulator.step();
+    }
+
+    this.viz.update(this.simulator);
+}
+
+
